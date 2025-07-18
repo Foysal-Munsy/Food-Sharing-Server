c61ff2ed963cac993cb9a2d6aa1bb55e782ff68d
Foysal-Munsy <bdfoysal555@gmail.com>
2025-07-18 23:40:13 +0600

    delete api

--- index.js
+++ index.js
@@ -129,6 +129,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/foods/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodCollection.deleteOne(query);
+      res.send(result);
+    });
+
     console.log("DB connected!");
   } finally {
   }
