a788e47296b24727aacc96155d4a1c47133229fe
Foysal-Munsy <bdfoysal555@gmail.com>
2025-10-27 00:19:29 +0600

    fix: load firebase admin-key.json using fs instead of import assertion

--- services/firebaseService.js
+++ services/firebaseService.js
@@ -1,6 +1,14 @@
 // services/firebaseService.js
 import admin from "firebase-admin";
-import serviceAccount from "../admin-key.json" assert { type: "json" };
+import fs from "fs";
+import path from "path";
+
+// Get absolute path to JSON
+const __dirname = path.resolve();
+const serviceAccountPath = path.join(__dirname, "admin-key.json");
+
+// Read and parse JSON
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf-8"));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
