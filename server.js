a4b74506139f87af2a7c7b066d1a98ec067ffc40
Foysal-Munsy <bdfoysal555@gmail.com>
2025-10-27 00:01:36 +0600

    chore: setup base project structure and rename index.js to server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
